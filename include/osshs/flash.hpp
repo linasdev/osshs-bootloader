dd6b9e807c5d99c26c7d28b9591643652164fecc
Linas Nikiperavicius <linas@linasdev.com>
2019-12-09 16:11:46 +0200

    Implement simple flash operations

---
+++ include/osshs/flash.hpp
@@ -0,0 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Linas Nikiperavicius
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef OSSHS_FLASH_HPP
+#define OSSHS_FLASH_HPP
+
+#include <cstdint>
+
+#define KEY_RDPRT 0x00A5
+#define KEY1 			0x45670123
+#define KEY2 			0xCDEF89AB
+
+namespace osshs
+{
+	class Flash
+	{
+	public:
+		/**
+		 * @brief Check if FLASH_CR register is locked. Writing to flash is only possible if it is unlocked.
+		 * @return Whether or not the FLASH_CR register is locked.
+		 */
+		static bool
+		isLocked();
+		
+		/**
+		 * @brief Unlock FLASH_CR register. Writing to flash is only possible after unlocking.
+		 * @return Whether or not unlocking succeeded.
+		 */
+		static bool
+		unlock();
+
+		/**
+		 * @brief Lock FLASH_CR register. Writing to flash will not be possible after locking.
+		 */
+		static void
+		lock();
+
+		/**
+		 * @brief Read a value from flash.
+		 * @param address Address where the value should be read from.
+		 * @return Value read from flash.
+		 */
+		static uint16_t
+		readHalfWord(uint32_t address);
+
+		/**
+		 * @brief Write a balue to flash.
+		 * @param address Address where the value should be written to.
+		 * @param value Value to write.
+		 * @return Whether or not writing succeeded.
+		 */
+		static bool
+		writeHalfWord(uint32_t address, uint16_t value);
+	};
+}
+
+#endif  // OSSHS_FLASH_HPP
---
+++ src/osshs/flash.cpp
@@ -0,0 +1,90 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Linas Nikiperavicius
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <osshs/log/logger.hpp>
+#include <osshs/flash.hpp>
+#include <modm/platform.hpp>
+
+namespace osshs
+{
+		bool
+		Flash::isLocked()
+		{
+			return FLASH->CR & FLASH_CR_LOCK;
+		}
+
+		bool
+		Flash::unlock()
+		{
+			OSSHS_LOG_INFO("Unlocking flash.");
+
+			FLASH->KEYR = KEY1;
+			FLASH->KEYR = KEY2;
+
+			if (isLocked())
+			{
+				OSSHS_LOG_ERROR("Unlocking flash failed.");
+				return false;
+			}
+
+			OSSHS_LOG_INFO("Unlocking flash succeeded.");
+			return true;
+		}
+
+		void
+		Flash::lock()
+		{
+			OSSHS_LOG_INFO("Locking flash.");
+
+			FLASH->CR |= FLASH_CR_LOCK;
+		}
+
+		uint16_t
+		Flash::readHalfWord(uint32_t address)
+		{
+			uint16_t value = *reinterpret_cast<uint16_t *>(address);
+
+			OSSHS_LOG_DEBUG("Reading half word from flash(address = `%x`, value = `%x`).", address, value);
+
+			return value;
+		}
+
+		bool
+		Flash::writeHalfWord(uint32_t address, uint16_t value)
+		{
+			OSSHS_LOG_DEBUG("Writing half word to flash(address = `%x`, value = `%x`).", address, value);
+
+			FLASH->CR |= FLASH_CR_PG;
+
+			*reinterpret_cast<volatile uint16_t *>(address) = value;
+
+			while(FLASH->SR & FLASH_SR_BSY);
+
+			if (readHalfWord(address) == value)
+				return true;
+
+			OSSHS_LOG_ERROR("Writing half word to flash failed(address = `%x`, value = `%x`).", address, value);
+			return false;
+		}
+}
